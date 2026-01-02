8c9f3ca0ff30076b5c0b460969cfda2730f53786
fawzanoachira <fawzanoachira@gmail.com>
2026-01-02 09:10:09 +0530

    user resposne modified

--- src/main/java/com/library/library_api/user/controller/UserAPIController.java
+++ src/main/java/com/library/library_api/user/controller/UserAPIController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.library.library_api.user.dto.CreateUserDto;
 import com.library.library_api.user.dto.UpdateUserDto;
+import com.library.library_api.user.dto.UserResponseDto;
 import com.library.library_api.user.exception.NoUserFoundException;
 import com.library.library_api.user.model.User;
 import com.library.library_api.user.service.UserService;
@@ -32,36 +33,40 @@ public class UserAPIController {
     @PostMapping("create")
     public ResponseEntity<HashMap<String, Object>> createUser(@RequestBody @Validated CreateUserDto createUserDto) {
         User user = userService.createUser(createUserDto.toUser());
+        UserResponseDto userResponseDto = new UserResponseDto(user);
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("msg", "User created successfully");
-        hashMap.put("data", user);
+        hashMap.put("data", userResponseDto);
         return new ResponseEntity<>(hashMap, HttpStatus.CREATED);
     }
 
     @GetMapping("get-user")
     public ResponseEntity<HashMap<String, Object>> getUser(@RequestParam String email) throws NoUserFoundException {
         User user = userService.getUserByEmail(email);
+        UserResponseDto userResponseDto = new UserResponseDto(user);
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("msg", "User fetched successfully");
-        hashMap.put("data", user);
+        hashMap.put("data", userResponseDto);
         return new ResponseEntity<>(hashMap, HttpStatus.OK);
     }
 
     @PutMapping("update-user")
     public ResponseEntity<HashMap<String, Object>> updateUser(@RequestBody @Validated UpdateUserDto updateUserDto){
         User updateUser = userService.updateUser(updateUserDto.toUser());
+        UserResponseDto userResponseDto = new UserResponseDto(updateUser);
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("msg", "User updated successfully");
-        hashMap.put("data", updateUser);
+        hashMap.put("data", userResponseDto);
         return new ResponseEntity<>(hashMap, HttpStatus.OK);
     }
 
     @DeleteMapping("delete-user/{userId}")
     public ResponseEntity<HashMap<String, Object>> deleteUser(@PathVariable UUID userId) throws NoUserFoundException{
         User deleteUser = userService.deleteUser(userId);
+        UserResponseDto userResponseDto = new UserResponseDto(deleteUser);
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("msg", "User deleted successfully");
-        hashMap.put("data", deleteUser);
+        hashMap.put("data", userResponseDto);
         return new ResponseEntity<>(hashMap, HttpStatus.OK);
     }
 }
